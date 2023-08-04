336b3aaeb235261113108ae6520478a4184155c7
unknown <mrianhricuk@gmail.com>
2023-08-04 23:55:35 +0300

    new correction

--- src/Main.java
+++ src/Main.java
@@ -6,15 +6,14 @@ public class Main {
 
     public static void main(String[] args) {
         List<String> name = List.of("Ivan", "Dima", "sofia", "Andriy", "Ivan", "Peter", "Mary", "John", "Alice");
-        String nams = evennames(name);
-        String nameOne = "1." + name.get(1) + ".";
-        System.out.println(nameOne + nams);
+
+        System.out.println(evennames(name));
     }
 
     public static String evennames(List<String> name) {
         return IntStream.range(0, name.size())
                 .filter(i -> i % 2 != 0)
-                .mapToObj(i -> (i + 1) + "." + name.get(i))
-                .collect(Collectors.joining("."));
+                .mapToObj(i -> i + "." + name.get(i))
+                .collect(Collectors.joining(", "));
     }
 }
--- src/taskTwo/Tasko.java
+++ src/taskTwo/Tasko.java
@@ -6,11 +6,10 @@ import java.util.stream.Collectors;
 
 public class Tasko {
     public static void main(String[] args) {
-        List<String> word=List.of("son","alvo","dano","andreq");
-      List<String> sorte= word.stream()
-                .map(i-> i.toUpperCase())
+        List<String> word = List.of("son", "alvo", "dano", "andreq");
+        System.out.println(word.stream()
+                .map(String::toUpperCase)
                 .sorted(Comparator.reverseOrder())
-               .peek(System.out::println)
-              .collect(Collectors.toList());
+                .collect(Collectors.toList()));
     }
 }
